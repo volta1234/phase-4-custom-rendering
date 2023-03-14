1e3e9427a0e635676a79aa1c8f8a500bf1f832c9
CALVINCE <calvince.ogwel@gmail.com>
2023-03-14 22:13:48 +0300

    custom rendering code along

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,24 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
-  def index
-    cheeses = Cheese.all
-    render json: cheeses
-  end
+def index
+  cheeses = Cheese.all
+  render json: cheeses, except: [:created_at, :updated_at]
+end
 
   # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+def show
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    # status: :not_found will produce a 404 status code
+    render json: { error: 'Cheese not found' }, status: :not_found
+  end
+end
+
+  def summary
+    "#{name}: $#{price}"
   end
 
 end
